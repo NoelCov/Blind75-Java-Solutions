1abc4141eadf6bccf206803ff7d1c614036a7ffa
Noel <no3lcodes@gmail.com>
2023-06-01 09:13:32 -0700

    Added solution to 'MergeTwoSortedLinkedLists' problem

---
+++ LinkedLists/MergeTwoSortedLinkedLists/Solution.java
@@ -0,0 +1,45 @@
+// Time Complexity O(n)
+// Space Complexity O(1)
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummyNode = new ListNode();
+        ListNode curr = dummyNode;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+                curr = curr.next;
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+                curr = curr.next;
+            }
+        }
+
+        if (list1 != null) {
+            curr.next = list1;
+        } else if (list2 != null) {
+            curr.next = list2;
+        }
+
+        return dummyNode.next;
+    }
+
+    class ListNode {
+        private int val;
+        private ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
