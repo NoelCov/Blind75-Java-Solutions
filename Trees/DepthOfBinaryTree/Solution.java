19d94ad081ee07b4a3ca2b8ec762c08addd6419b
Noel <no3lcodes@gmail.com>
2023-06-04 11:21:36 -0700

    Added solution to 'MaximumDepthOfBinaryTree' problem

---
+++ Trees/DepthOfBinaryTree/Solution.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+// Time Complexity O(V + E)
+// Space Complexity O(V + E)
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = maxDepth(root.left) + 1;
+        int right = maxDepth(root.right) + 1;
+
+        return Math.max(left, right);
+    }
+}
